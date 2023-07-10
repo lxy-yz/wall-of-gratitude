4a19c68bde321b847099eaf5330d38efd081ee21
lxy-yz <lxy.dev.yz@gmail.com>
2023-07-10 12:15:04 -0700

    discover page w/ tags

---
+++ app/discover/filters.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { Badge } from "@/components/ui/badge"
+import { useRouter, useSearchParams } from "next/navigation"
+
+export const Filters = ({ tags }: {
+  tags: { id: string, name: string }[]
+}) => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  return (
+    <div className="mt-8 space-x-2">
+      <Badge
+        onClick={() => {
+          const newSearchParams = new URLSearchParams(searchParams)
+          newSearchParams.delete('tag')
+          router.push(`/discover?${newSearchParams}`)
+        }}
+        variant="outline"
+        className="cursor-pointer"
+      >
+        all
+      </Badge>
+      {tags.map(tag => (
+        <Badge
+          onClick={() => {
+            const newSearchParams = new URLSearchParams(searchParams)
+            newSearchParams.set('tag', tag.name)
+            router.push(`/discover?${newSearchParams}`)
+          }}
+          key={tag.id}
+          variant="outline"
+          className="cursor-pointer"
+        >
+          {tag.name}
+        </Badge>
+      ))}
+    </div>
+  )
+}
--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,9 +1,18 @@
+import { Badge } from "@/components/ui/badge";
+import { db } from "@/lib/db";
+import { Filters } from "./filters";
+
 export async function DiscoverPage() {
+  const tags = await db.tag.findMany({
+    take: 10,
+  });
+
   return (
     <div className="mx-auto max-w-screen-lg">
       <h1 className="my-8 text-4xl font-bold tracking-tight">
         Discover
       </h1>
+      <Filters tags={tags} />
       <div className="space-y-1 mt-8">
         <h2 className="text-2xl font-semibold tracking-tight">
           Recent Sender
